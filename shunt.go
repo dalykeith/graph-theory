ca7406eb91a4d151c6d8224d31899733d6e8e78c
Keith <keithdaly11@hotmail.com>
2018-04-01 18:16:27 +0100

    updated code

--- shunt.go
+++ shunt.go
@@ -5,10 +5,35 @@ import (
 )
 
 func intopost(infix string) string {
+		specials := map[rune]int{'*': 10, '.': 9, '|': 8}
 
-	// speciaials := map[rune]int{'*': 10, '.': 9, '|': 8}
+	//pofix := []rune{}
+	pofix,  s := []rune{}, []rune{}
 
-	pofix := []rune{}
+	//Loop over input
+	for _, r := range infix {
+		switch {
+		case r == '(':
+		s = append(s, r)
+		case r == ')':
+			for s[len(s)-1] != '(' {
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)]
+			}
+			s = s[:len(s)-1]
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]]{
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			s = append(s, r)
+		default:
+			pofix = append(pofix,r)
+		}
+	}
+
+	for len(s) > 0 {
+		pofix, s = append(pofix,s[len(s)-1]), s[:len(s)-1]
+	}
+
 	return string(pofix)
 }
 func main(){
